5cd282bb7950885d310a4b263a4103792d333f78
minsss0726 <wndtmgld5551@gmail.com>
2025-09-26 11:59:14 +0900

    [minjung] fix : 스프링 서버 수정

--- srv/vite/src/shared/api/axiosInstance.jsx
+++ srv/vite/src/shared/api/axiosInstance.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import callToken from "../hook/callToken";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "", // 상대 경로로 변경
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://ohgoodteam.shinhanacademy.co.kr", // 직접 HTTPS로 연결
   timeout: 10000,
 
   headers: {
